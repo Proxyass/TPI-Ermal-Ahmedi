c84b6f8dd6a2d478439b5a08130c31ab5197aa4d
eaeahmedi <ermal.a14@hotmail.com>
2022-04-20 08:32:26 +0200

    Update User.cs

--- ErmalTpi/Model/User.cs
+++ ErmalTpi/Model/User.cs
@@ -8,11 +8,13 @@ namespace ErmalTpi.Model
 {
     public class User
     {
+        public string UID { get; set; }
         public int UserId { get; set; }
+        public int EntryType { get; set; }
         public string Nom1 { get; set; }
         public string Nom2 { get; set; }
         public string Prenom1 { get; set; }
         public string Prenom2 { get; set; }
-        public DateTime Date_Naissance { get; set; } = DateTime.Now;
+        public DateTime Date_Naissance { get; set; }
     }
 }
